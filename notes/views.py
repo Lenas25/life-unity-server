859987ff247afe5e5f2cef8b2e386ef48105b339
Julio Vargas <jvdark2020@gmail.com>
2024-07-26 18:01:01 -0500

    notes filter

--- notes/views.py
+++ notes/views.py
@@ -3,6 +3,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Notes
 from .serializers import NotesSerializer
 from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
 
 
 # Create your views here.
@@ -10,3 +11,5 @@ class NotesViewSet(ModelViewSet):
     queryset = Notes.objects.all()
     serializer_class = NotesSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['user_id']
